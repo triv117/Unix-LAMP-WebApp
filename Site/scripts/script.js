f48c008ec931c92cea6f74863e536f0983948d0e
triv117 <tomriv117@outlook.com>
2022-05-04 17:46:10 -0400

    Added new Files

---
+++ Site/scripts/script.js
@@ -0,0 +1,14 @@
+const userName = document.getElementsByClassName("username");
+const password = document.getElementsByClassName("password");
+
+function loginValidate(){
+    // const username = "unix";
+    // const passwd = "project";
+
+    // if(userName != "" && password != "" ){
+    //     if(userName == "unix" && password == "project"){
+    //         window.location.href = "../about.html";
+    //     }
+    // }
+
+}
